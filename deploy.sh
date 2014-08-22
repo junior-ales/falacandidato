e102a36b4398e427ce7a2b0bcb57f8ed3a3e4aee
Junior Ales <edilson.ales.jr@gmail.com>
2014-08-22 01:53:41 -0300

    Editando script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-# Esse script assume que tudo já esteja criado,
+# Esse script assume que o código já esteja desenvolvido,
 # só faltando fazer o push para o master e o deploy
-# para o branch gh-pages
+# para o branch gh-pages. Obviamente alguns métodos para
+# pegar erros não esperados foram incluídos
 
-#bundle exec jekyll build &&
-#git reset HEAD . &&
-#git add _site/feed.xml &&
-#git commit -m "Atualizando feed.xml" &&
-#git pull --rebase &&
+bundle exec jekyll build &&
+git reset HEAD . &&
+git add _site/feed.xml &&
+git commit -m "Atualizando feed.xml" &&
+git pull --rebase &&
 git push origin master &&
 git subtree push --prefix _site/ origin gh-pages
