e8f4ba7a854c7e4bfaa799a3a469ed187c4ddbfd
Junior Ales <edilson.ales.jr@gmail.com>
2014-08-28 20:25:56 -0300

    Deixando o layout mais responsivo

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 $(document).ready(function(){
-  $('.latest-post-content').slick({
-    centerMode: true,
-    centerPadding: '10px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 980,
-        settings: {
-          centerMode: false,
-          slidesToShow: 2
+  var enableSlick = function enableSlick() {
+    $('.latest-post-content').slick({
+      centerMode: true,
+      centerPadding: '10px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 980,
+          settings: {
+            centerMode: false,
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 660,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '35px',
+            slidesToShow: 1
+          }
         }
-      },
-      {
-        breakpoint: 660,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '35px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+      ]
+    });
+  };
+
+  var disableSlick = function disableSlick() {
+    $('.latest-post-content').unslick();
+  };
+
+  var isMobile = function isMobile() {
+    return $('#mobile-indicator').is(':visible');
+  };
+
+  (function slickHandler() {
+    isMobile() ? disableSlick() : enableSlick();
+  })();
 });
